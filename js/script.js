14475cd7374b149c88a1c7ce41bfdffa1b211af6
Andrei Kudrin <andreikudrin@gmail.com>
2020-09-17 11:52:29 +0100

    Finished Javascript and some styling

--- js/script.js
+++ js/script.js
@@ -1,56 +1,130 @@
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit");
+(function () {
+  //functions
+  function buildQuiz() {
+    // variable to store the HTML output
+    const output = [];
 
-function buildQuiz() {}
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // variable to store the list of possible answers
+      const answers = [];
 
-function showResults() {}
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+        );
+      }
 
-const myQuestions = [
-  {
-    question: "Jim .... every day.",
-    answers: {
-      a: "don't watch TV",
-      b: "go to school",
-      c: "reads a book",
-      d: "don't go to school",
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "green";
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `Answers correct ${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  //variables
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Jim .... every day.",
+      answers: {
+        a: "don't watch TV",
+        b: "go to school",
+        c: "reads a book",
+        d: "don't go to school",
+      },
+      correctAnswer: "c",
     },
-    correctAnswer: "c",
-  },
-  {
-    question: "Anna .... at the moment.",
-    answers: {
-      a: "read a book",
-      b: "reads a book",
-      c: "is reading a book",
-      d: "has read a book",
+    {
+      question: "Anna .... at the moment.",
+      answers: {
+        a: "read a book",
+        b: "reads a book",
+        c: "is reading a book",
+        d: "has read a book",
+      },
+      correctAnswer: "c",
     },
-    correctAnswer: "c",
-  },
-  {
-    question: "Mary .... already.",
-    answers: {
-      a: "read a book",
-      b: "have done her homework",
-      c: "has cleaned her room",
-      d: "drive to the city",
+    {
+      question: "Mary .... already.",
+      answers: {
+        a: "read a book",
+        b: "have done her homework",
+        c: "has cleaned her room",
+        d: "drive to the city",
+      },
+      correctAnswer: "c",
     },
-    correctAnswer: "c",
-  },
-  {
-    question: "Mary .... already.",
-    answers: {
-      a: "read a book",
-      b: "have done her homework",
-      c: "has cleaned her room",
-      d: "drive to the city",
+    {
+      question: "They .... live in Tallinn.",
+      answers: {
+        a: "lives",
+        b: "is living",
+        c: "live",
+        d: "doesn't live",
+      },
+      correctAnswer: "c",
     },
-    correctAnswer: "c",
-  },
-];
+    {
+      question: "We feel tired because we .... all day.",
+      answers: {
+        a: "walked",
+        b: "walk",
+        c: "have been walking",
+        d: "are walking",
+      },
+      correctAnswer: "c",
+    },
+  ];
 
-// display quiz right away
-buildQuiz();
+  // display quiz right away
+  buildQuiz();
 
-// on submit, show results
-submitButton.addEventListener("click", showResults);
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
